988edc20bc10bb5941505d1adcc32c9727956b44
JuiWen,Chiang <97574000+JuiWenChiang@users.noreply.github.com>
2023-09-13 06:57:02 +0000

    feat: download button

--- src/views/ResumView.vue
+++ src/views/ResumView.vue
@@ -1,7 +1,22 @@
 <template>
     <h>Resume</h>
+    <v-btn @click="downloadResume()">get Resume</v-btn>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, computed, watch, onMounted, type StyleValue } from 'vue'
+
+function downloadResume() {
+    let pdfUrl = '/JuiWenChiange_Resume.pdf'
+    const link = document.createElement('a');
+    link.href = pdfUrl;
+    link.target = '_blank';
+    link.download = 'Jui_Wen_Chiange_Resume.pdf';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+
+</script>
 
 <style lang="scss" scoped></style>
