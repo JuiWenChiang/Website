87a7eede5bc5bd8170413ca8e08c751545597721
JuiWen,Chiang <97574000+JuiWenChiang@users.noreply.github.com>
2023-09-14 07:57:40 +0000

    feat: icons hyperlink

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,7 +1,25 @@
 <template>
-    <h>Contact</h>
+    <h1>Contact</h1>
+    <v-icon icon="mdi-github" size="x-large" @click="openLink('github')"></v-icon>
+    <v-icon icon="mdi-linkedin" size="x-large" @click="openLink('linkedin')"></v-icon>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+
+function openLink(mode: string) {
+    let linkURL = '';
+    switch (mode) {
+        case 'github':
+            linkURL = 'https://github.com/JuiWenChiang';
+            break;
+        case 'linkedin':
+            linkURL = 'https://www.linkedin.com/in/juiwen-chiang1995/';
+            break;
+    }
+
+    window.open(linkURL, '_blank');
+}
+
+</script>
 
 <style lang="scss" scoped></style>
